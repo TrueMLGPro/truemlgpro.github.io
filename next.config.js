3d840d196cf2789750da3d76d50030bdb190e2d6
TrueMLGPro <revit005@gmail.com>
2023-06-07 16:52:09 +0300

    🔧 Removed API folder, removed unused config lines

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
 module.exports = { pageExtensions: ['js'], reactStrictMode: true }
--- pages/api/public_ip.js
+++
@@ -1,6 +0,0 @@
-import requestIp from 'request-ip';
-
-export default function handler(req, res) {
-  const ipAddress = requestIp.getClientIp(req);
-  res.status(200).json({ ip: ipAddress });
-}
